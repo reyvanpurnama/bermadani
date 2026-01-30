de687fc9910f9973abcdaf94be46895b6fe5ac34
reyvan.evan <vant8forbid@gmail.com>
2026-01-31 03:45:32 +0700

    fix: remove duplicate alpine.js import in membership layout to resolve livewire data hydration issues

--- resources/views/layouts/membership.blade.php
+++ resources/views/layouts/membership.blade.php
@@ -14,7 +14,6 @@
     <!-- Scripts -->
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/qrious/4.0.2/qrious.min.js"></script>
-    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     @livewireStyles
     @stack('styles')
 
